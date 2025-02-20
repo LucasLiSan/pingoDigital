e7f06e8b1337fb384b1ff0bfdf35010739e5efb0
LucasLiSan <lukassantana_rgt@hotmail.com>
2025-02-20 17:51:04 -0300

    layout achados e perdidos

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /* ----------  IMPORTAÇÃO DE MÓDULOS ---------- */
 import express from "express";
 import ip from "ip";
-import mongoose from "mongoose";
+//import mongoose from "mongoose";
 import multer from "multer";
 
 /* ----------  BANCO DE DADOS ---------- */
-//import mongoose from "./config/dbConfig.js";
-mongoose.connect("mongodb://127.0.0.1:27017/api-pingoDigital");
+import mongoose from "./config/dbConfig.js";
+//mongoose.connect("mongodb://127.0.0.1:27017/api-pingoDigital");
 
 /* ----------  IMPORTAÇÃO DE MODELS ---------- */
 import Student from "./models/students.js";
@@ -23,6 +23,7 @@ import sectorRoutes from "./routes/sectorStreetsRoutes.js"
 import sobreRoutes from "./routes/sobreRoute.js";
 import studentRoutes from "./routes/studentsRoutes.js";
 import transparenciaRoutes from "./routes/transparenciaRoutes.js";
+import { render } from "ejs";
 
 /* ----------  INICIANDO O EXPRESS ---------- */
 const app = express();
@@ -48,10 +49,10 @@ app.use('/', transparenciaRoutes);
 /* ----------\/ INICIANDO SERVIDOR \/---------- */
 const port = 8080;
 const myServer = ip.address();
-//const renderPort = '0.0.0.0';
+const renderPort = '0.0.0.0';
 console.log(myServer);
 
-app.listen(port, (error) => {
+app.listen(port, renderPort, (error) => {
     if(error) {console.log(error); }
     console.log(`API rodando em http://localhost:${port}.`);
 });
